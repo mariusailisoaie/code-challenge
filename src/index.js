c7af3c10885b7b3a3b8fc62ebee87f67d4f27ee2
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-08-14 09:10:41 +0200

    Add support for the colors white and black

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const { getColor } = require('./apiMock');
 
-async function getColors(green, blue, red, order, callback) {
+function getColors(green, blue, red, black, white, order, callback) {
 	const colors = [];
 	if (green) colors[order.indexOf('green')] = getColor('green');
 	if (blue) colors[order.indexOf('blue')] = getColor('blue');
 	if (red) colors[order.indexOf('red')] = getColor('red');
+	if (black) colors[order.indexOf('black')] = getColor('black');
+	if (white) colors[order.indexOf('white')] = getColor('white');
 	callback(colors);
 	return colors;
 }
 
 function colors() {
-	let green = process.argv[2];;
-	let blue = process.argv[3]
-	let red = process.argv[4];
-	const colorOrder = process.argv[5]
-	getColors(green, blue, red, JSON.parse(colorOrder), async function (colors) {
+	let black, white;
+
+	if (process.argv.indexOf('black') !== -1) black = process.argv.indexOf('black');
+	if (process.argv.indexOf('white') !== -1) white = process.argv.indexOf('white');
+
+	let green = process.argv.indexOf('green');
+	let blue = process.argv.indexOf('blue');
+	let red = process.argv.indexOf('red');
+	const colorOrder = process.argv[process.argv.length - 1];
+	getColors(green, blue, red, black !== undefined && black, white !== undefined && white, JSON.parse(colorOrder), async function (colors) {
 		colors = await Promise.all(colors)
 		colors.forEach(color => console.log(`${ color.name } ${ color.HEX } rgb(${ color.RGB.R }, ${ color.RGB.G }, ${ color.RGB.B })`));
 	});
